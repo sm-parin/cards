64566fa16d8a4e561648f17fe08ef9f50b422721
Parin SM <sm.parin@gmail.com>
2026-04-05 12:25:35 +0530

    Remove temp migration script

--- apps/server/run-migrate.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-process.env.DATABASE_URL = 'postgresql://neondb_owner:npg_mGMvSWB1ayK7@ep-weathered-morning-a1s9cdqx.ap-southeast-1.aws.neon.tech/neondb?sslmode=require';
-const { runMigrations } = require('./src/db/migrate');
-runMigrations()
-  .then(() => { console.log('MIGRATIONS OK'); process.exit(0); })
-  .catch(err => { console.error('MIGRATION FAILED:', err.message); process.exit(1); });
